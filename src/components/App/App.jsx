cb92a3081f9b8ba6125ac21af4a7d404a3eb750b
Tony LaForgia <alforgia428@gmail.com>
2022-02-17 16:41:30 -0600

    Created getList function in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-
-import Header from '../Header/Header.jsx'
-import './App.css';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import Header from "../Header/Header.jsx";
+import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  const [groceryList, setGroceryList] = useState([]);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = () => {
+    axios
+      .get("/list")
+      .then((response) => {
+        setGroceryList(response.data);
+      })
+      .catch((error) => {
+        console.log("getList error", error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
