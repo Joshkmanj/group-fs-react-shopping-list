8ba10c6f6c3dbc06f351e4a490ee80bab780df2a
Josh Kralewski <joshkmanj@gmail.com>
2022-02-17 13:56:58 -0600

    Database setup added

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- Database name: fs-react-shopping
+
+CREATE TABLE "shopping-list" (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR(80),
+"quantity" NUMERIC(3,2),
+"unit" VARCHAR(20),
+"purchase-status" BOOLEAN DEFAULT FALSE
+);
