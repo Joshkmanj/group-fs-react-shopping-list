079b2197915ac2d26dae7f901d61a8d8c38a31fe
Tony LaForgia <alforgia428@gmail.com>
2022-02-17 16:20:03 -0600

    Router GET sqlText written. .then() written

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const pool = require("../modules/pool.js");
 
 // TODO - Add routes here...
-
+router.get("/", (req, res) => {
+  const sqlText = `SELECT * FROM "shopping-list" ORDER BY "purchase-status", "name" ASC;`;
+  pool.query(sqlText).then((result) => {
+    console.log("Router GET TEST", result);
+  });
+});
 
 module.exports = router;
