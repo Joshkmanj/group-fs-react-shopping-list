f3c01d5a9571a5ba612c3ee949272e590e77060c
Leah Atkins <leah.atkins84@gmail.com>
2022-02-17 16:53:00 -0600

    router post started

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,42 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+
+
+
+
+
+
+
+router.post('/', (req, res) => {
+    const newItemInfo = req.body;
+    console.log(newItemInfo);
+    const sqlText = `
+    INSERT INTO "shopping-list" ("name", "quantity", "unit")
+    VALUES ($1, $2, $3);`
+    const itemDetails = [newItemInfo.name, newItemInfo.quantity, newItemInfo.unit]
+
+    pool.query(sqlText, itemDetails)
+        .then(result => {
+            res.sendStatus(201);
+        }).catch(err => {
+            res.sendStatus(500);
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+    const addItem = () => {
+        axios.post('/list', {})
+    }
+    
     return (
         <div className="App">
             <Header />
