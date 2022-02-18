a74a09bd69c765c7565e0eb2cb47c4cd753db956
Leah Atkins <leah.atkins84@gmail.com>
2022-02-18 09:26:36 -0600

    saving to work to on later

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
 
     const addItem = () => {
-        axios.post('/list', {})
+        axios.post('/list', {itemInfoList: itemInfoList})
     }
     
     return (
