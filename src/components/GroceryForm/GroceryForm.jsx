097cc666c2365c2c09d45c7e3317a359c2fd0046
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-02-18 10:28:31 -0600

    form inputs set values and send those to post route, post route still to be made

--- src/components/GroceryForm/GroceryForm.jsx
+++ src/components/GroceryForm/GroceryForm.jsx
@@ -1,10 +1,30 @@
-function GroceryForm(params) {
+import React, { useState } from 'react';
+
+function GroceryForm({taco}) {
+
+    const [item, setItem] = useState('')
+    const [quantity, setQuantity] = useState('')
+    const [unit, setUnit] = useState('')
+
+
+    const handleSubmit = () => {
+        console.log(item);
+        taco(item, quantity, unit);
+        clearFormInputs()
+    }
+
+    const clearFormInputs = () => {
+        setItem('')
+        setQuantity('')
+        setUnit('')
+    }
+
     return (
         <>
-             <form >
+             <form onSubmit={handleSubmit}>
                 <h2>Add an Item</h2>
                 <input autoFocus onChange={(event) => setItem(event.target.value)}
-                    // value={student.github_name}
+                    // figure these out late --> value={student.github_name}
                     placeholder="Itme" />
                 <input onChange={(event) => setQuantity(event.target.value)}
                     // value={student.github_name}
@@ -12,6 +32,7 @@ function GroceryForm(params) {
                 <input onChange={(event) => setUnit(event.target.value)}
                     // value={student.github_name}
                     placeholder="DUh units" />
+                <input type="submit" value="Submit" />
             </form>
         </>
     )
